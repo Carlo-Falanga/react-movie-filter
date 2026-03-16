02108b3a2ab9d4ca9bf589bf2ef8e0231ce01442
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-16 11:29:08 +0100

    add movies list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react"
+
+ const movies = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+
 
 function App() {
 
 
   return (
     <>
+    <div className="container">
+      <ul>
+      {
+        movies.map((item, index) =>(
+          
+          <li key={index}>{item.title}, {item.genre}</li>
+        ))
+      }
 
+      </ul>
+    </div>
     </>
   )
 }
