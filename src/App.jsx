8c7eebaea947ce9b9a06cfbefe78522a46a6074a
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-16 11:36:22 +0100

    add select options

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
- const movies = [
-   { title: 'Inception', genre: 'Fantascienza' },
-   { title: 'Il Padrino', genre: 'Thriller' },
-   { title: 'Titanic', genre: 'Romantico' },
-   { title: 'Batman', genre: 'Azione' },
-   { title: 'Interstellar', genre: 'Fantascienza' },
-   { title: 'Pulp Fiction', genre: 'Thriller' },
- ]
-
+const movies = [
+  { title: "Inception", genre: "Fantascienza" },
+  { title: "Il Padrino", genre: "Thriller" },
+  { title: "Titanic", genre: "Romantico" },
+  { title: "Batman", genre: "Azione" },
+  { title: "Interstellar", genre: "Fantascienza" },
+  { title: "Pulp Fiction", genre: "Thriller" },
+];
 
 function App() {
-
-
   return (
     <>
-    <div className="container">
-      <ul>
-      {
-        movies.map((item, index) =>(
-          
-          <li key={index}>{item.title}, {item.genre}</li>
-        ))
-      }
-
-      </ul>
-    </div>
+      <div className="container">
+        <select className="form-select my-5" aria-label="Default select example">
+          <option>All</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+        <ul className="list-group">
+          {movies.map((item, index) => (
+            <li className="list-group-item" key={index}>
+              {item.title}, {item.genre}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
